99837fb6014657f6bafdaf6764980482a1c61ac9
FerroEduardo <47820549+ferroeduardo@users.noreply.github.com>
2020-12-14 17:28:32 -0300

    start project

---
+++ src/main/java/com/ferroeduardo/cartorios_api_frontend/CartoriosApiFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.ferroeduardo.cartorios_api_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartoriosApiFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartoriosApiFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ferroeduardo/cartorios_api_frontend/CartoriosApiFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferroeduardo.cartorios_api_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartoriosApiFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
